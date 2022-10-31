5e428de5cf2e233eba08c30464fb186cd9093689
cronree-91 <64413934+cronree-91@users.noreply.github.com>
2022-11-01 08:26:45 +0900

    update keymaps

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,9 +5,7 @@ alias ls="ls --color -a"
 function fish_user_key_bindings
   bind \cr 'fzf_select_history (commandline -b)'
   bind \cf fzf_select_ghq_repository
-  bind \cp 'fzf_select_file_to_edit'
-  bind \co 'fzf_select_cd'
-  bind \cl clear
+  bind \co fzf_select_file_to_edit
 end
 
 if test -e /etc/os-release
@@ -15,3 +13,7 @@ if test -e /etc/os-release
     alias pbcopy="xsel --clipboard --input"
     alias pbpaste="xsel --clipboard --output"
 end
+
+set EDITOR /usr/sbin/nvim
+
+alias keymap="echo 'Ctrl+R History';echo 'Ctrl+F ghq repo';echo 'Ctrl+O Open a file with editor';echo 'Ctrl+T Find File';echo 'Alt+C  sub-dir'"
