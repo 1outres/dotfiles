8d061235a228f86e57a9c1f105c4464eac5fe45b
cronree-91 <64413934+cronree-91@users.noreply.github.com>
2023-08-11 22:06:48 +0900

    better screenshot.sh

--- .config/screenshot.sh
+++ .config/screenshot.sh
@@ -1,9 +1,9 @@
 #!/bin/env bash
 
-currenddir=$(pwd)
 cd ~/Screenshots
+
+GRIM_DEFAULT_DIR="$HOME/Screenshots"
 
 grim -g "$(slurp -w 0)"
 wl-copy < $(pwd)/$(ls --ignore={.,..} -1 -t | head -n 1)
 
-cd $currentdir
