1bae08270fba5f3b6c8222a44a2c640da7e21654
cronree-91 <64413934+cronree-91@users.noreply.github.com>
2023-10-30 10:08:04 +0900

    onfig fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,35 +1,38 @@
 alias l="lazygit"
 alias vim="nvim"
 alias ls="ls --color -a"
-alias sed="gsed"
 
 if test -e /etc/os-release
-    # Linux
-    alias pbcopy="xsel --clipboard --input"
-    alias pbpaste="xsel --clipboard --output"
+  # Linux
+  alias pbcopy="xsel --clipboard --input"
+  alias pbpaste="xsel --clipboard --output"
+end
+
+if test (uname -s) = "Darwin"
+  eval (/opt/homebrew/bin/brew shellenv)
+  alias sed="gsed"
 end
 
 set -x EDITOR $(which nvim)
 set -x M3_HOME /opt/maven
 
-eval (/opt/homebrew/bin/brew shellenv)
 
 alias keymap="echo 'Ctrl+R History';echo 'Ctrl+F ghq repo';echo 'Ctrl+O Open a file with editor';echo 'Ctrl+T Find File';echo 'Alt+C  sub-dir'"
 
 function attach_tmux_session_if_needed
-    if test -z (tmux list-sessions)
-        tmux new-session
-        return
-    end
+  if test -z (tmux list-sessions)
+    tmux new-session
+    return
+  end
 
-    set ID (tmux list-sessions | sed '1iCreate New Session' | fzf | cut -d: -f1)
-    if test "$ID" = "Create New Session"
-        tmux new-session
-    else if test -n "$ID"
-        tmux attach-session -t "$ID"
-    end
+  set ID (tmux list-sessions | sed '1iCreate New Session' | fzf | cut -d: -f1)
+  if test "$ID" = "Create New Session"
+    tmux new-session
+  else if test -n "$ID"
+    tmux attach-session -t "$ID"
+  end
 end
 
 if test -z $TMUX && status --is-login
-    attach_tmux_session_if_needed
+  attach_tmux_session_if_needed
 end
