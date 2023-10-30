871c6ca8f6f7a23e633e5f384200e5d21b594511
cronree-91 <64413934+cronree-91@users.noreply.github.com>
2023-10-30 09:52:26 +0900

    tmux

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,4 +16,20 @@ eval (/opt/homebrew/bin/brew shellenv)
 
 alias keymap="echo 'Ctrl+R History';echo 'Ctrl+F ghq repo';echo 'Ctrl+O Open a file with editor';echo 'Ctrl+T Find File';echo 'Alt+C  sub-dir'"
 
+function attach_tmux_session_if_needed
+    if test -z (tmux list-sessions)
+        tmux new-session
+        return
+    end
 
+    set ID (tmux list-sessions | sed '1iCreate New Session' | fzf | cut -d: -f1)
+    if test "$ID" = "Create New Session"
+        tmux new-session
+    else if test -n "$ID"
+        tmux attach-session -t "$ID"
+    end
+end
+
+if test -z $TMUX && status --is-login
+    attach_tmux_session_if_needed
+end
