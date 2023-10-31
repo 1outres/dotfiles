3de604bc05cdfbcc9efa344e7580030b1741c464
cronree-91 <64413934+cronree-91@users.noreply.github.com>
2023-10-31 09:58:32 +0900

    ssh, datefolder

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,18 +21,32 @@ alias keymap="echo 'Ctrl+R History';echo 'Ctrl+F ghq repo';echo 'Ctrl+O Open a f
 
 function attach_tmux_session_if_needed
   if test -z (tmux list-sessions)
-    tmux new-session
-    return
+    set ID (echo SSH DevServer\nCreate New Session | fzf | cut -d: -f1)
+  else if test (uname -s) = "Darwin"
+    set ID (tmux list-sessions | sed '1iSSH DevServer\nCreate New Session' | fzf | cut -d: -f1)
+  else 
+    set ID (tmux list-sessions | sed '1iCreate New Session' | fzf | cut -d: -f1)
   end
-
-  set ID (tmux list-sessions | sed '1iCreate New Session' | fzf | cut -d: -f1)
   if test "$ID" = "Create New Session"
     tmux new-session
+  else if test "$ID" = "SSH DevServer"
+    ssh dev.cron.home
   else if test -n "$ID"
     tmux attach-session -t "$ID"
   end
 end
 
+function create_date_folder
+  set DATE (date -Idate | sed -e "s/-/\//g")
+  if ! test -d "$HOME/Documents/$DATE"
+    mkdir -p "$HOME/Documents/$DATE"
+    echo "Created today's folder!"
+  end
+  cd "$HOME/Documents/$DATE/"
+end
+
 if test -z $TMUX && status --is-login
   attach_tmux_session_if_needed
+else
+  create_date_folder
 end
