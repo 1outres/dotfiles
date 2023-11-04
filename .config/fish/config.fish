bb122c324237a90c1b8cb859ad8a942833c73c79
cronree-91 <64413934+cronree-91@users.noreply.github.com>
2023-11-04 18:35:23 +0900

    gpg

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,6 +16,9 @@ end
 set -x EDITOR $(which nvim)
 set -x M3_HOME /opt/maven
 
+set -x GPG_TTY $(tty)
+gpgconf --launch gpg-agent
+set -x SSH_AUTH_SOCK $(gpgconf --list-dirs agent-ssh-socket)
 
 alias keymap="echo 'Ctrl+R History';echo 'Ctrl+F ghq repo';echo 'Ctrl+O Open a file with editor';echo 'Ctrl+T Find File';echo 'Alt+C  sub-dir'"
 
