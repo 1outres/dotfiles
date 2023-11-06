2315662b9c0d4484659baae206ef1fcae8e43706
cronree-91 <cron@cron.jp>
2023-11-06 15:04:44 +0900

    cd tmux

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -22,23 +22,6 @@ set -x SSH_AUTH_SOCK $(gpgconf --list-dirs agent-ssh-socket)
 
 alias keymap="echo 'Ctrl+R History';echo 'Ctrl+F ghq repo';echo 'Ctrl+O Open a file with editor';echo 'Ctrl+T Find File';echo 'Alt+C  sub-dir'"
 
-function attach_tmux_session_if_needed
-  if test -z (tmux list-sessions)
-    set ID (echo SSH DevServer\nCreate New Session | fzf | cut -d: -f1)
-  else if test (uname -s) = "Darwin"
-    set ID (tmux list-sessions | sed '1iSSH DevServer\nCreate New Session' | fzf | cut -d: -f1)
-  else 
-    set ID (tmux list-sessions | sed '1iCreate New Session' | fzf | cut -d: -f1)
-  end
-  if test "$ID" = "Create New Session"
-    tmux new-session
-  else if test "$ID" = "SSH DevServer"
-    ssh dev.cron.home
-  else if test -n "$ID"
-    tmux attach-session -t "$ID"
-  end
-end
-
 function create_date_folder
   set DATE (date -Idate | sed -e "s/-/\//g")
   if ! test -d "$HOME/Documents/$DATE"
@@ -48,8 +31,17 @@ function create_date_folder
   cd "$HOME/Documents/$DATE/"
 end
 
+function cd
+  if test "$argv[1]" = "today"
+    create_date_folder
+  else
+    builtin cd $argv
+  end
+end
+
 if test -z $TMUX && status --is-login
   attach_tmux_session_if_needed
 else
   create_date_folder
 end
+
