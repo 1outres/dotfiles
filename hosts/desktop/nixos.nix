7778c58143ad93cb44a742bfabd83d2dc9f8f395
Loutres <me@loutres.me>
2024-09-14 20:55:06 +0900

    add: nixos-hardware

--- hosts/desktop/nixos.nix
+++ hosts/desktop/nixos.nix
@@ -7,18 +7,24 @@
 }:
 
 {
-  imports = [
-    ../../modules/core/i18n.nix
-    ../../modules/core/nix.nix
-    ../../modules/core/user.nix
-    ../../modules/programs/hyprland.nix
-    ../../modules/programs/shell.nix
-    ../../modules/programs/ssh.nix
-    ../../modules/desktop/fonts.nix
-    ../../modules/desktop/i18n.nix
+  imports =
+    [
+      ../../modules/core/i18n.nix
+      ../../modules/core/nix.nix
+      ../../modules/core/user.nix
+      ../../modules/programs/hyprland.nix
+      ../../modules/programs/shell.nix
+      ../../modules/programs/ssh.nix
+      ../../modules/desktop/fonts.nix
+      ../../modules/desktop/i18n.nix
 
-    ./hardware-configuration.nix
-  ];
+      ./hardware-configuration.nix
+    ]
+    ++ (with inputs.nixos-hardware.nixosModules; [
+      common-cpu-amd
+      common-gpu-nvidia
+      common-pc-ssd
+    ]);
 
   boot = {
     loader = {
