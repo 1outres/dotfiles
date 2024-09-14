423d718845906df3e4c0fb1baf16c129b740c7e0
loutres <me@loutres.me>
2024-09-15 02:22:05 +0900

    fix: settings

--- hosts/vaio/user.nix
+++ hosts/vaio/user.nix
@@ -14,15 +14,17 @@
   ];
   wayland.windowManager.hyprland.settings = {
     monitor = [
-      "eDP-1, 3840x2160@60, 0x0, 1"
+      "eDP-1, 3840x2160@60, 0x0, 1.5"
     ];
     input = {
       kb_layout = "us";
       sensitivity = -0.2;
     };
   };
-  programs.waybar.settings[0].battery = {
-    bat = "BAT1";
-    full-at = 100;
-  };
+  programs.waybar.settings = [{
+    battery = {
+      bat = "BAT1";
+      full-at = 100;
+    };
+  }];
 }
