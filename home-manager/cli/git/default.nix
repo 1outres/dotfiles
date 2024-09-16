0ed26cab484cdfed864d66d623d78fed17d085cb
loutres <me@loutres.me>
2024-09-16 16:09:04 +0900

    fix

--- home-manager/cli/git/default.nix
+++ home-manager/cli/git/default.nix
@@ -4,5 +4,9 @@
     enable = true;
     userName = "loutres";
     userEmail = "me@loutres.me";
+
+    extraConfig = {
+      url."git@github.com:mNi-Cloud/backend.git".insteadOf = "https://github.com/mNi-Cloud/backend";
+    };
   };
 }
--- home-manager/cli/packages/default.nix
+++ home-manager/cli/packages/default.nix
@@ -3,10 +3,11 @@
   home.packages = with pkgs; [
     fzf ghq lazygit go
     ffmpeg htop 
-    unzip zip
+    unzip zip wget
     grim slurp python3
     screen xclip neofetch tty-clock cava
     jq yq-go
     kubectl
+    speedtest-cli
   ];
 }
