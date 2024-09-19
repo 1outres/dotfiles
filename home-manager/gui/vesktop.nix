6dd67e114c015665d6592a165e92443529c05159
loutres <me@loutres.me>
2024-09-19 14:12:51 +0900

    add: desktop entry for vesktop

--- home-manager/gui/vesktop.nix
+++ home-manager/gui/vesktop.nix
@@ -3,4 +3,13 @@
   home.packages = with pkgs; [
     vesktop
   ];
+
+  xdg.desktopEntries.vesktop = {
+    name = "Vesktop";
+    exec = "${pkgs.vesktop.outPath}/bin/vesktop --enable-features=UseOzonePlatform --ozone-platform=wayland --enable-wayland-ime %U";
+    terminal = false;
+    type = "Application";
+    icon = "vesktop";
+    categories = ["Network" "InstantMessaging" "Chat"];
+  };
 }
