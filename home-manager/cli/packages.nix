337822fbbf2a12dbbe62e66fd1378b05d2a25847
loutres <me@loutres.me>
2024-09-28 20:43:35 +0900

    add: trash-cli

--- home-manager/cli/packages.nix
+++ home-manager/cli/packages.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    fzf ghq lazygit go ffmpeg htop unzip zip wget grim slurp python3 screen xclip neofetch tty-clock cava jq yq-go kubectl speedtest-cli cloudflared dig lsof tcpdump whois xsel nodejs_20
+    fzf ghq lazygit go ffmpeg htop unzip zip wget grim slurp python3 screen xclip neofetch tty-clock cava jq yq-go kubectl speedtest-cli cloudflared dig lsof tcpdump whois xsel nodejs_20 trash-cli
   ];
 }
